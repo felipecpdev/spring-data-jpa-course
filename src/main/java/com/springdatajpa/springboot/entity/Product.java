94c093cbc6142115224ad2e6ae99d9670efae024
Felipe Contreras <felipe.contreras.dev@gmail.com>
2023-06-01 19:22:37 -0400

    fin seccion 3.

--- src/main/java/com/springdatajpa/springboot/entity/Product.java
+++ src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -1,11 +1,21 @@
 package com.springdatajpa.springboot.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(
         name = "product",
         uniqueConstraints = {
@@ -26,7 +36,9 @@ public class Product {
     private BigDecimal price;
     private boolean active;
     private String imageUrl;
+    @CreationTimestamp
     private LocalDateTime dateCreated;
+    @UpdateTimestamp
     private LocalDateTime lastUpdated;
 
 }
