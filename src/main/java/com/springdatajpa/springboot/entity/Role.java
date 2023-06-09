1ec7d7ae84d327e3de40e76398b9d71c8bc9c7f4
Felipe Contreras <felipe.contreras.dev@gmail.com>
2023-06-09 16:41:52 -0400

    ManytoMany Mapping bidireccional.fin seccion 14

--- src/main/java/com/springdatajpa/springboot/entity/Role.java
+++ src/main/java/com/springdatajpa/springboot/entity/Role.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
 @Entity
@@ -14,4 +17,8 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER, mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
 }
