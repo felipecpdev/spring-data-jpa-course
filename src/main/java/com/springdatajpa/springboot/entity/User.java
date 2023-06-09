83d710dbc8e1d10be775d281df169a9e356e60ed
Felipe Contreras <felipe.contreras.dev@gmail.com>
2023-06-09 16:32:16 -0400

    ManytoMany Mapping unidireccional

---
+++ src/main/java/com/springdatajpa/springboot/entity/User.java
@@ -0,0 +1,35 @@
+package com.springdatajpa.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users",
+        uniqueConstraints = @UniqueConstraint(name = "unique_mail", columnNames = "email"))
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id",referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id",referencedColumnName = "id"
+            )
+    )
+    private Set<Role> roles= new HashSet<>();
+}
